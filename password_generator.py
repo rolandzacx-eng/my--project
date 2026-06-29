f6dc1e6ea1914f81b3dcb7671b87f0f806390236
rolandzacx-eng <rolandzacx@gmail.com>
2026-06-29 11:12:47 +0300

    project 3:Password Generator with strength checker

---
+++ password_generator.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits +string.punctuation
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+    return password
+def get_strength(length):
+    if length <8:
+        return "weak"
+    elif length <12:
+        return "medium"
+    else:
+        return "strong"
+
+length = int(input("Enter password length: "))
+count = int(input("How many passwords do you want? "))
+
+print("\n--- Your Generated Passwords   ---")
+for i in range(count):
+    pwd = generate_password(length)
+    strength = get_strength(length)
+    print(f"{i+1}. {pwd}  [{strength}]")
